32e979d253f3d7a06553fe01387110fd68d161de
rizki.sasri <rizki.sasri@mplus.software>
2023-06-25 15:47:04 +0700

    update index

--- boards/views.py
+++ boards/views.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
 
-@method_decorator([login_required], name="dispatch")
+# @method_decorator([login_required], name="dispatch")
 class AccountIndexView(TemplateView):
     template_name = "account/index.html"
 
